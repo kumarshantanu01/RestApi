111d3e335954efbbe45a5105c65368c83fefef04
kumarshantanu01 <shantanuk280@gmail.com>
2021-11-18 17:38:33 +0530

    defining schema and model

---
+++ src/models/students.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: [true, 'already registered'],
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Invalid email')
+            }
+        }
+    },
+    phone:{
+        type:Number,
+        min:10,
+        // max:10,
+        required: true,
+        unique: true
+    } ,
+    address:{
+        type: String,
+        required:true
+    }
+})
+
+//creating new collection
+const Student = new mongoose.model('Student', studentSchema);
+
+module.exports = Student;
