9d171378e3033ed9332eb3f19fe4804e72facffe
kumarshantanu01 <shantanuk280@gmail.com>
2021-11-18 17:39:00 +0530

    connecting to postman

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 const express = require('express');
+require("./db/connection");
+const Student = require('./models/students');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res)  =>{
-    res.send('hello shantanu');
-})
+app.use(express.json());
 
-app.get('/students', (req, res)  =>{
-    res.send('hello students');
+// app.post('/', (req, res)  =>{
+//     res.send('hello kumar shantanu');
+// })
+
+//to create new students id
+app.post('/students', (req, res)  =>{
+    console.log(req.body);
+    const user = new Student(req.body);
+
+    user.save().then( () =>{
+        res.status(201).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+
+    // res.send('hello students');
 })
 
 app.listen(port, () => {
